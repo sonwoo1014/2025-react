8a28a1ddae3501fd387c155f5202f75eca46b674
손우진 <sonwoo1014@handong.ac.kr>
2025-11-13 12:16:42 +0900

    20251113

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <div>I Love JSX!<div>hahah!!</div></div>  //JSX
+const myElement = <div style ={{color: "blue"}}>I Love JSX!<div>hahah!!</div></div>  //JSX
 const myElement1 = React.createElement('h1', {}, 'I do not use JSX!');
 const myElement2 = <div>React is {5 + 5} times better with JSX</div>;
 
